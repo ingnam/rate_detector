a20129bfa3aff75960ef794360f8a031dd0b82f7
Madan Ingnam <madingnm@gmail.com>
2022-07-22 09:54:51 +1000

    initial commit

---
+++ lib/rate_detector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rate_detector/version"
+
+module RateDetector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rate_detector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RateDetector
+  VERSION = "0.1.0"
+end
